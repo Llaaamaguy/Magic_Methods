912d1864a8bf6f0d39ee9e520d193427a71a8f15
Llaaamaguy <llamaguy.8267@gmail.com>
2020-09-22 15:14:48 -0400

    Update Magic.py
    Wont run

---
+++ Magic.py
@@ -0,0 +1,25 @@
+from math import gcd
+
+class Fraction:
+    
+    def __init__(self, n, d):
+        self.n = n
+        self.d = d
+
+    def reduce(self):
+        d = gcd(self.n, self.d)
+
+        n = self.n // d
+        d = self.d // d
+
+        return (n, d)
+
+def main():
+
+    n1 = 16
+    n2 = 10
+    fract = Fraction(n1, n2)
+    print('The fraction is {}'.format(fract.reduce()))
+
+if __name__ == "__main__":
+    main() 

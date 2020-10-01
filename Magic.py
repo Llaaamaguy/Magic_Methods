99076150b34786cd44df734b15a8672351561726
Llaaamaguy <llamaguy.8267@gmail.com>
2020-10-01 14:56:36 -0400

    Update Magic.py

--- Magic.py
+++ Magic.py
@@ -14,14 +14,63 @@ class Fraction:
 
         return (n, d)
 
+    def __add__(self, other):
+        self.n
+        self.d
+        other.n
+        other.d
+        NNum = self.n + other.n
+        NDenom = self.d + other.d
+        return Fraction(NNum, NDenom)  
+    
+    def __sub__(self, other):
+        self.n
+        self.d
+        other.n
+        other.d
+        NNum = self.n - other.n
+        NDenom = self.d - other.d
+        return Fraction(NNum, NDenom)
+    
+    def __mul__(self, other):
+        self.n
+        self.d
+        other.n
+        other.d
+        NNum = self.n * other.n
+        NDenom = self.d * other.d
+        return Fraction(NNum, NDenom)
+
+    def __div__(self, other):
+        self.n
+        self.d
+        other.n
+        other.d
+        NNum = self.n * other.d
+        NDenom = self.d * other.n 
+        return Fraction(NNum, NDenom)
+
+    def __str__(self):
+        return "{} / {}".format(self.n, self.d)
+    
+    def __float__(self):
+        return self.n / self.d
+
     
 
 def main():
 
     n1 = 16
     n2 = 10
-    fract = Fraction(n1, n2)
-    print('The fraction is {}'.format(fract.reduce()))
+    n3 = 10
+    n4 = 20
+    fract1 = Fraction(n1, n2)
+    fract2 = Fraction(n3, n4)
+    print('The reduced fraction is {}'.format(fract1.reduce()))
+    print('The reduced fraction is {}'.format(fract2.reduce()))
+    added = fract1 + fract2
+    print('Fraction 1 plus fraction 2 is {}'.format(added.reduce()))
+
 
 if __name__ == "__main__":
     main() 
